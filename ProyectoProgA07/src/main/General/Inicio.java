f995a2efdf40fb430a418c320fb5f9de60de0437
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2021-11-08 14:52:52 +0100

    Clase inicio creada, sera desde donde se crea la pagina de inicio del pacman y se inicie el juego

---
+++ ProyectoProgA07/src/main/General/Inicio.java
@@ -0,0 +1,19 @@
+package main.General;
+
+import javax.swing.JFrame;
+
+public class Inicio extends JFrame{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private void initUI() {
+		setTitle(" Pac-Man");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
+		setSize(385, 420);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+
+}
