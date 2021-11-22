5c5ca4256381e72b72bbb7550089058452c8148e
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2021-11-22 08:36:48 +0100

    Inicio para crear la base de daos

--- ProyectoProgA07/src/main/bd/BD.java
+++ ProyectoProgA07/src/main/bd/BD.java
@@ -1,10 +1,37 @@
 package main.bd;
 
+
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
 public class BD {
+	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	private static Logger logger = null;
+	
+	public static void setLogger(Logger logger) {
+		BD.logger = logger;
 	}
+	
+	//metodo para logear
+	private static void log( String msg, Throwable excepcion) {
+		if (logger == null) { // Logger por defecto 
+			logger = Logger.getLogger("BD-server"); // Nombre del logger
+			try {
+				logger.addHandler(new FileHandler("bd.log.xml", true)); // Saca el log a fichero xml
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, "No se pudo crear fichero de log", e);
+			}
+		}
+		if (excepcion == null)
+			logger.log(null, msg);
+		else
+			logger.log(null, msg, excepcion);
+	}
+	
+
 
 }
