d266211b5d5bbb56c3b2f45633d68580fb1fb7e7
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2021-11-30 15:20:08 +0100

    CAMBIOS NO SUBIDOS - Creacion de las tablas en la base de datos y quitados unos errores que salian sobre los niveles, mas adelante los solucionare

--- ProyectoProgA07/src/main/bd/BD.java
+++ ProyectoProgA07/src/main/bd/BD.java
@@ -19,12 +19,12 @@ public class BD {
 			if (connection == null) {
 				Class.forName("org.sqlite.JDBC");
 				connection = DriverManager.getConnection("jdbc:sqlite:" + nombreBD);
-				log(Level.INFO, "Conectada base de datos " + nombreBD, null);
+				log( "Conectada base de datos " + nombreBD, null);
 			}
 			return connection;
 		} catch (ClassNotFoundException | SQLException e) {
 			lastError = e;
-			log(Level.SEVERE, "Error en conexión de base de datos " + nombreBD, e);
+			log("Error en conexión de base de datos " + nombreBD, e);
 			e.printStackTrace();
 			return null;
 		}
@@ -53,6 +53,37 @@ public class BD {
 			logger.log(null, msg, excepcion);
 	}
 	
+	//Creacion de tablas
+	public static Statement creacionTablas(Connection con) {
+	
+		try {
+			Statement statement;
+			statement = con.createStatement();
+			statement.setQueryTimeout(10);
+			statement.executeUpdate("create table if not exists usuario "
+					+ "(nick string primary key, password string not null, fechaNacimiento integer, genero string)");
+			statement.executeUpdate("create table if not exists pacman" + "(idJuego integer primary key, pacman string not null)");
+			statement.executeUpdate("create table if not exists participacion "
+					+ "(idParticipacion Integer primary key AUTOINCREMENT, " + "nick string not null,"
+					+ "nivel integer not null, " + "idJuego integer not null,"
+					+ "puntuacion integer not null, fecha integer," + "foreign key(idJuego) references pacman(idJuego),"
+					+ "foreign key(nick) references usuario(nick))");
+			statement.executeUpdate("create table if not exists nivel " + "(idJuego  integer not null," + "nick string not null,"
+					+ "maxNivel integer not null," + " foreign key(idJuego) references pacman(idJuego),"
+					+ " foreign key(nick) references participacion(nick))");
 
+			//Informamos de que la base de datos ha sido creada
+			log("Creada base de datos", null);
+			return statement;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			lastError= e;
+			log("Error en creación de base de datos", e);
+			e.printStackTrace();
+			return null;
+		}
+		
+		
+	}
 
 }
