de9667f3227895f3950fb22702a6f6050c47e7ed
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2021-12-01 14:30:17 +0100

    CAMBIOS NO SUBIDOS - Añadidos algunos detalles a la ventana Inicio para poder conectar con otras ventanas y poder lanzar el juego

--- ProyectoProgA07/src/main/general/Inicio.java
+++ ProyectoProgA07/src/main/general/Inicio.java
@@ -5,12 +5,18 @@ import javax.swing.JFrame;
 public class Inicio extends JFrame{
 	
 	private static final long serialVersionUID = 1L;
+	private int nivel = 1;
+	
+	public Inicio (String usuario) {
+		Const.usuarioLogeado = usuario;
+	}
 	
 	private void initUI() {
+		//add(new Tablero);
 		setTitle(" Pac-Man");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-		setSize(385, 420);
+		setSize(360, 420);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
