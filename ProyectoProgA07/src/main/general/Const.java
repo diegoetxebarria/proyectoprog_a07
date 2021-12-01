4134c68e5e11e6174aec559c818ee081fd5fea33
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2021-12-01 14:42:31 +0100

    CAMBIOS NO SUBIDOS - Añadido constantes que necesitaremos en la clase Tablero

--- ProyectoProgA07/src/main/general/Const.java
+++ ProyectoProgA07/src/main/general/Const.java
@@ -3,5 +3,12 @@ package main.general;
 public class Const {
 	
 	static String usuarioLogeado;
+	
+	static int nivel = 1;
+	static int contNiv = 6;
+
+	static int[] contadNiv;
+	
+	final static int niveles[] = { 1, 2, 3, 4, 5 };
 
 }
