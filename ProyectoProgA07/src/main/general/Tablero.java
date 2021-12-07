3f281c696b02b2addd68ce06e22bfe92f186f4e5
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2021-12-07 14:44:21 +0100

    Añadidos diferentes metodos, como el de inicio de variables, el de mover el pacman que usaremos para mover el pacman

--- ProyectoProgA07/src/main/general/Tablero.java
+++ ProyectoProgA07/src/main/general/Tablero.java
@@ -1,5 +1,12 @@
 package main.general;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.util.Date;
+
+
+
 
 //import main.bd.BD;
 
@@ -24,7 +31,7 @@ public class Tablero {
 	public int[] colorParedesFantasmas = { 182, 30, 24 };
 	public int tamanyoRed = 30;
 
-	protected static int[][] tablero = {
+	protected static int[][] tabler = {
 			{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
 			{ 1, 2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 1 },
 			{ 1, 2, 1, 2, 1, 1, 2, 1, 2, 1, 2, 1, 1, 1, 1, 2, 1, 1, 2, 1, 1, 1, 1, 2, 1, 2, 1, 2, 1, 1, 2, 1, 2, 1 },
@@ -71,7 +78,96 @@ public class Tablero {
 	public void seleccionNivel() {
 		setNivel(Const.nivel);
 	}
+	
+	
+	
+	private void iniciarVariable() {
+		Const.colorLab = new Color(5, 100, 5);
+		Const.dimension = new Dimension(400, 400);
+		Const.fantx = new int[Const.velMaxFant];
+		Const.fantdx = new int[Const.velMaxFant];
+		Const.fanty = new int[Const.velMaxFant];
+		Const.fantdy = new int[Const.velMaxFant];
+		Const.fantvel = new int[Const.velMaxFant];
+		Const.dx = new int[4];
+		Const.dy = new int[4];
 
+		Const.timer.start();
+	}
+	
+	private void jugar(Graphics2D g2d) {
+		if (Const.muerteEnYa) {
+			asesinato();
+		} else {
+
+			moverPacman();
+			//Faltan metodos 
+		}
+	}
+	
+	//Metodo para mover le pacman
+	private void moverPacman() { 
+
+		int pos;
+		short ch;
+
+		if (Const.exdx == -Const.pacmandx && Const.exdy == -Const.pacmandy) {
+			Const.pacmandx = Const.exdx;
+			Const.pacmandy = Const.exdy;
+			Const.vistadx = Const.pacmandx;
+			Const.vistady = Const.pacmandy;
+		}
+
+		if (Const.pacmanx % Const.tamanobloque == 0 && Const.pacmany % Const.tamanobloque == 0) {
+			pos = Const.pacmanx / Const.tamanobloque + Const.bloques * (int) (Const.pacmany / Const.tamanobloque);
+			ch = Const.screendata[pos];
+
+			if ((ch & 16) != 0) {
+				Const.screendata[pos] = (short) (ch & 15);
+				Const.resultado++;
+
+			}
+
+			if (Const.exdx != 0 || Const.exdy != 0) {
+				if (!((Const.exdx == -1 && Const.exdy == 0 && (ch & 1) != 0) || (Const.exdx == 1 && Const.exdy == 0 && (ch & 4) != 0)
+						|| (Const.exdx == 0 && Const.exdy == -1 && (ch & 2) != 0) || (Const.exdx == 0 && Const.exdy == 1 && (ch & 8) != 0))) {
+					Const.pacmandx = Const.exdx;
+					Const.pacmandy = Const.exdy;
+					Const.vistadx = Const.pacmandx;
+					Const.vistady = Const.pacmandy;
+				}
+			}
+
+			if ((Const.pacmandx == -1 && Const.pacmandy == 0 && (ch & 1) != 0) || (Const.pacmandx == 1 && Const.pacmandy == 0 && (ch & 4) != 0)
+					|| (Const.pacmandx == 0 && Const.pacmandy == -1 && (ch & 2) != 0)
+					|| (Const.pacmandx == 0 && Const.pacmandy == 1 && (ch & 8) != 0)) {
+				Const.pacmandx = 0;
+				Const.pacmandy = 0;
+
+			}
+		}
+		Const.pacmanx = Const.pacmanx + Const.velocidadpacman * Const.pacmandx;
+		Const.pacmany = Const.pacmany + Const.velocidadpacman * Const.pacmandy;
+	}
+	
+	
+	private void asesinato() {// Cuando lo asesinen los fantasmas
+
+		Const.vidaspacman--;
+
+		if (Const.vidaspacman == 0) {
+
+			Date fecha = new Date();
+			//Guardar datos falta meter
+		}
+
+		continuarNivel();
+	}
+	
+
+	private void continuarNivel() {
+		//Falta crear metodo
+	}
 	public int contarPuntos(int[][] tablero) {
 
 		int cantidadPuntos = 0;
