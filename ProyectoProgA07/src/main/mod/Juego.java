0d82299da44390dbe25996b0bb3a467e22b3800f
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2022-01-10 15:19:20 +0100

    Nueva clase Juego con las caracteristicas del juego

---
+++ ProyectoProgA07/src/main/mod/Juego.java
@@ -0,0 +1,19 @@
+package main.mod;
+
+public class Juego {
+
+	Integer id;
+	
+	public Juego(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+}
