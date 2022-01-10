bf561bf3bd6be04b803ea74f2cff757cdf6aeb76
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2022-01-10 15:19:29 +0100

    Nueva clase Nivel con las caracteristicas del nivel

---
+++ ProyectoProgA07/src/main/mod/Niveles.java
@@ -0,0 +1,39 @@
+package main.mod;
+
+public class Niveles {
+
+	Integer id;
+	Integer nivel;
+	Integer juegoId;
+
+	public Niveles(Integer id, Integer nivel, Integer juegoId) {
+		this.id = id;
+		this.nivel = nivel;
+		this.juegoId = juegoId;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Integer getNivel() {
+		return nivel;
+	}
+
+	public void setNivel(Integer nivel) {
+		this.nivel = nivel;
+	}
+
+	public Integer getJuegoId() {
+		return juegoId;
+	}
+
+	public void setJuegoId(Integer juegoId) {
+		this.juegoId = juegoId;
+	}
+	
+}
