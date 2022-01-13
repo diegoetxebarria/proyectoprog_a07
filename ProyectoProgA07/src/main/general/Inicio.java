f46167958d06a4b16a1384b17c30449bf3a887f2
diegoetxebarria <48069203+diegoetxebarria@users.noreply.github.com>
2022-01-13 15:48:53 +0100

    Ultimos cambios desde la ventana de inicio del juego pacman, ya no da errorres

--- ProyectoProgA07/src/main/general/Inicio.java
+++ ProyectoProgA07/src/main/general/Inicio.java
@@ -1,25 +1,93 @@
 package main.general;
 
-import javax.swing.JFrame;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.logging.Level;
 
-public class Inicio extends JFrame{
-	
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+import main.bd.BD;
+import main.mod.Juego;
+import main.ventanas.VentanaLogin;
+
+public class Inicio extends JFrame implements Juego{
+
 	private static final long serialVersionUID = 1L;
 	private int nivel = 1;
-	
-	public Inicio (String usuario) {
+
+	public Inicio(String usuario) {
 		Const.usuarioLogeado = usuario;
 	}
-	
+
 	private void initUI() {
-		//add(new Tablero);
-		setTitle(" Pac-Man");
+
+		add(new Tablero(this, nivel));
+		setTitle("Juego PacMan");
+		setIconImage(Toolkit.getDefaultToolkit()
+				.getImage(this.getClass().getResource("/img/Imagenes_sueltas/pacman-favicon.png")));
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setDefaultCloseOperation( JFrame.DISPOSE_ON_CLOSE );
-		setSize(360, 420);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setSize(385, 420);
 		setResizable(false);
 		setLocationRelativeTo(null);
 		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+		} catch (Exception e) {
+		} //
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				Inicio ex = new Inicio(Const.usuarioLogeado);
+				ex.initUI();
+				ex.setVisible(true);
+			}
+		});
+	}
+
+	public void jugar() {
+		initUI();
+		Const.log.log(Level.INFO, "Se inicia el juego");
+		this.setVisible(true);
+
+	}
+
+	public void setNivel(int level) {
+		Const.log.log(Level.INFO, "Se establece el nivel " + level + " al juego pacman");
+		if (level < 1 || level > 6) {
+			Const.log.log(Level.SEVERE, "El valor del nivel tiene que estar entre 1 y 6");
+			throw new IllegalArgumentException("El valor del nivel tiene que estar entre 1 y 6");
+		}
+
+		this.nivel = level;
+
+	}
+
+	public void guardarPuntuacion(String nick, Date fecha, int nivel, int puntuacion) {
+		Connection con = BD.initBD(VentanaLogin.NOMBRE_BD);
+		Statement st = BD.usarBD(con);
+		BD.participacionInsert(st, nick, 2, nivel, puntuacion, fecha.getTime());
+	}
+
+	public void guardarNuevoMaxNivel(Integer idJuego, Integer nivel, String nick) {
+		Connection con = BD.initBD(VentanaLogin.NOMBRE_BD);
+		Statement sta = BD.usarBD(con);
+		String sql = "update nivel set maxNivel = maxNivel +1 where idJuego =2 and nick = '" + nick
+				+ "' and maxNivel < 6 and maxNivel = " + nivel;
+		try {
+			sta.executeUpdate(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
